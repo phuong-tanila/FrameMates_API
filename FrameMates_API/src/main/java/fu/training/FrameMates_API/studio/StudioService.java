84cbb846787afc6cdd23df2680cebaac9cb4a1f4
Long56642002 <longhoangdinh212@gmail.com>
2023-06-16 02:28:30 +0700

    implement search studio by name

--- FrameMates_API/src/main/java/fu/training/FrameMates_API/studio/StudioService.java
+++ FrameMates_API/src/main/java/fu/training/FrameMates_API/studio/StudioService.java
@@ -3,6 +3,9 @@ package fu.training.FrameMates_API.studio;
 import fu.training.FrameMates_API.share.exceptions.RecordNotFoundException;
 import fu.training.FrameMates_API.studio.Studio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface StudioService {
     long count();
 
@@ -10,4 +13,5 @@ public interface StudioService {
 
     StudioModel getById(Integer id);
     StudioModel updateStatus(Integer id, Integer status);
+    List<StudioModel> getByName(String name);
 }
