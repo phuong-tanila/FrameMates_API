a5494396000fec8c122a8d1e271858cd7acb5745
Long56642002 <70355608+Long56642002@users.noreply.github.com>
2023-06-24 16:34:24 +0700

    Revert "Delete voucher API (#8)"
    This reverts commit 2b6ad213cf2fd219dbf1878ca57981abd9daa015.

--- FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherService.java
+++ FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherService.java
@@ -1,6 +1,4 @@
 package fu.training.FrameMates_API.voucher;
 
 public interface VoucherService {
-    VoucherModel create(VoucherModel model);
-    VoucherModel delete(Integer id);
 }
--- FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherServiceImpl.java
+++ FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherServiceImpl.java
@@ -3,35 +3,10 @@ package fu.training.FrameMates_API.voucher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class VoucherServiceImpl implements VoucherService {
-	@Autowired
-	private VoucherMapper voucherMapper;
+
 	@Autowired
 	private VoucherRepository voucherRepository;
-	private Voucher getVoucher(Integer id) {
-		Voucher voucher = null;
-		Optional optionalStudio = voucherRepository.findById(id);
-		if (optionalStudio.isPresent()) {
-			voucher = (Voucher) optionalStudio.get();
-		}
-		return voucher;
-	}
 
-	@Override
-	public VoucherModel create(VoucherModel model) {
-		return voucherMapper.toModel(voucherRepository.save(voucherMapper.toEntity(model)));
-	}
-
-	@Override
-	public VoucherModel delete(Integer id) {
-		Voucher voucher = getVoucher(id);
-		if (voucher == null) {
-			return null;
-		}
-		voucherRepository.delete(voucher);
-		return voucherMapper.toModel(voucher);
-	}
 }
