a3759c54df9964300a3c9208b439df2ddf1860a6
Long56642002 <70355608+Long56642002@users.noreply.github.com>
2023-06-26 09:48:00 +0700

    [Feature] Voucher module (#4)
    * implement voucher module - create api
    
    * implement delete voucher api (#11)

--- FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherMapper.java
+++ FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherMapper.java
@@ -3,8 +3,12 @@ package fu.training.FrameMates_API.voucher;
 import fu.training.FrameMates_API.voucher.Voucher;
 import fu.training.FrameMates_API.voucher.VoucherModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "spring")
+@Mapper(
+        componentModel = "spring",
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
+)
 public interface VoucherMapper {
 
     Voucher toEntity (VoucherModel model);
--- FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherService.java
+++ FrameMates_API/src/main/java/fu/training/FrameMates_API/voucher/VoucherService.java
@@ -1,4 +1,6 @@
 package fu.training.FrameMates_API.voucher;
 
 public interface VoucherService {
+    VoucherModel create(VoucherModel model);
+    VoucherModel delete(Integer id);
 }
