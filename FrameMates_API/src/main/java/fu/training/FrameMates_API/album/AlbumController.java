f86552d57e8504e98d5e16b55355d3efae7d7beb
phuong-tanila <nguyenphuong040217@gmail.com>
2023-07-08 14:30:10 +0700

    fix missing typo in album controller

--- FrameMates_API/src/main/java/fu/training/FrameMates_API/album/AlbumController.java
+++ FrameMates_API/src/main/java/fu/training/FrameMates_API/album/AlbumController.java
@@ -2,10 +2,14 @@ package fu.training.FrameMates_API.album;
 
 import fu.training.FrameMates_API.account.Account;
 import fu.training.FrameMates_API.album.AlbumService;
+import fu.training.FrameMates_API.servicepack.ServicePackModel;
+import fu.training.FrameMates_API.share.exceptions.RecordNotFoundException;
 import jakarta.validation.Valid;
 import jdk.jshell.spi.ExecutionControl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +34,7 @@ public class AlbumController {
 		return albumService.createAlbum(model, currentAccount.getEmployee());
 	}
 	@PreAuthorize("hasRole('EMPLOYEE')")
-	@DeleteMapping("${id}")
+	@DeleteMapping("{id}")
 	public AlbumModel deleteAlbum(
 			@PathVariable int id,
 			Authentication authentication
